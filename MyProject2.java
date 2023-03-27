6779e5a4c6fa2136f5aa026557631a8704ae833f
Saichon <jsaichon@gmail.com>
2023-03-27 14:02:49 +0700

    1 command

---
+++ MyProject2.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package myproject2;
+
+/**
+ *
+ * @author Student
+ */
+public class MyProject2 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
